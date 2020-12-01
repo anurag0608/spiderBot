82e172e2ac6d5aa4280fe0c9f87f168d27bb0d4f
Anurag Verma <anuragv.1020@gmail.com>
2020-12-02 02:05:32 +0530

    able to get meta info from websites **except pvr :)**

--- spiderbot.js
+++ spiderbot.js
@@ -4,9 +4,24 @@ const axios = require("axios"),
 
 async function findAllLinks(url, linkArr){
     let page = await axios.get(url).catch(err=> { throw err})
-    
         const $ = cheerio.load(page.data)
         const anchors = $('a')
+        // Getting titles, headings and bold texts //
+        const pageTitle = $('title');
+        let keywords = []
+        
+        if($('meta[name="keywords"]').attr('content')!=undefined) keywords = $('meta[name="keywords"]').attr('content').split(',')
+
+        let meta = {
+            keywords,
+            description : $('meta[name="description"]').attr('content')
+        }
+        let index = {
+            title: $('title').text(),
+            meta
+        }
+        console.log(index)
+        
         let count = 0
         $(anchors).each(function(i, anchor){
             let string = $(anchor).attr('href')
@@ -28,12 +43,12 @@ const display = (links)=>{
     })
 }
 async function main(){
-    let Urls = ["https://edition.cnn.com/","https://timesofindia.indiatimes.com/","https://www.ndtv.com/"]
-    const links = []
+    let Urls = ["https://in.bookmyshow.com/","https://www.pvrcinemas.com/","https://www.blu-ray.com/","https://www.youtube.com/"]
+    let links = []
     for(let i=0;i<Urls.length;i++){
         let url = Urls[i]
         if(url[url.length-1]=='/') url = url.slice(0, url.length-1)
-        await findAllLinks(url, links).catch(err=>console.log(err.code))
+        await findAllLinks(url, links).catch(err=>console.log("Error : " + err))
     }
 
     // display(links)
